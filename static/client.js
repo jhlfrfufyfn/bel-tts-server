6969224d60fb94974daa054edd18855d8dc75b73
jhlfrfufyfn <jhlfrfufyfn@gmail.com>
2022-03-28 15:42:12 +0300

    fix: improved js fronted logic

--- static/client.js
+++ static/client.js
@@ -1,21 +1,27 @@
 const button = document.getElementById('submit-text');
 
-const hostname = 'nikuchin.fun';
+const NODE_ENV = "development";
+
+const protocol = (NODE_ENV === "production") ? "https" : "http";
+const hostname = (NODE_ENV === "production") ? 'nikuchin.fun' : 'localhost:3000';
+
 button.addEventListener("click", function (e) {
     const submitted_text = document.getElementById('submitting_form').value;
+    const loader = document.getElementsByClassName("loader")[0];
+    loader.style.setProperty('display', 'block');
     axios({
-        url: `http://${hostname}/tts`,
+        url: `${protocol}://${hostname}/tts`,
         method: 'post',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         data: {
             text: submitted_text
         }
-    })
-    .then(response => {
+    }).then(response => {
         const filename = response.data.audio_name;
         const audio = new Audio(filename);
         console.log(audio);
         audio.controls = 'controls';
+        ///audio.preload = 'auto';
         audio.id = 'audio-player';
         const parent_element = document.getElementById("media-div");
         const audio_element = document.getElementById(audio.id);
@@ -24,10 +30,11 @@ button.addEventListener("click", function (e) {
             audio_element.parentNode.removeChild(audio_element);
         }
         parent_element.appendChild(audio);
-    })
-    .catch(error => {
+    }).catch(error => {
         const er_mes = error.message;
         console.log('error in getting response with the file name: ' + er_mes)
-    });
+    }).finally(() => {
+        loader.style.setProperty('display', 'none');
+    })
 
 });
