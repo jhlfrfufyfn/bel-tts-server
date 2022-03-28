5f861b8a2a9c4a90b04dcd28f0352c2181c3ad1c
jhlfrfufyfn <jhlfrfufyfn@gmail.com>
2022-03-28 15:41:50 +0300

    feat: commented https server config

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,32 @@
 import http from 'node:http';
 import https from 'node:https';
+import fs from 'fs';
 import { app } from './app';
 
 // Get port from environment and store in Express.
 const port = process.env.PORT || '3000';
-const secure_port = process.env.SECURE_PORT || '433';
-
 // Create HTTP server.
 const server = http.createServer(app);
 server.listen(port)
 console.log(`HTTP server listening on port ${port}`);
 
-// Create HTTPS server.
-const https_server = https.createServer(app);
-https_server.listen(secure_port)
-console.log(`HTTPS server listening on port ${secure_port}`);
+
+// Get port from environment and store in Express.
+/*
+if (process.env.NODE_ENV === "production") {
+    const securePort = process.env.SECURE_PORT || '3433';
+
+    const secretKeyPath = process.env.SECRET_KEY_PATH || "sslcert/server.key";
+    const certificatePath = process.env.CERTIFICATE_PATH || "sslcert/server.crt";
+
+    const privateKey = fs.readFileSync(secretKeyPath, 'utf8');
+    const certificate = fs.readFileSync(certificatePath, 'utf8');
+
+    const credentials = { key: privateKey, cert: certificate };
+
+    // Create HTTPS server.
+    const https_server = https.createServer(credentials, app);
+    https_server.listen(securePort)
+    console.log(`HTTPS server listening on port ${securePort}`);
+}
+*/
