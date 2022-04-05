e35b6b6b0ceefce8157956342fef2ddb66049ffe
jhlfrfufyfn <jhlfrfufyfn@gmail.com>
2022-04-06 00:03:50 +0300

    feat: alerts and text processing added

--- static/client.js
+++ static/client.js
@@ -1,20 +1,77 @@
+import { processText } from "./modules/process-text.js";
+
 const button = document.getElementById('submit-text');
 
-const NODE_ENV = "production";
+const NODE_ENV = "development";
 
 const protocol = (NODE_ENV === "production") ? "https" : "http";
 const hostname = (NODE_ENV === "production") ? 'nikuchin.fun' : 'localhost:3000';
 
-button.addEventListener("click", function (e) {
-    const submitted_text = document.getElementById('submitting_form').value;
+function showLoader() {
     const loader = document.getElementsByClassName("loader")[0];
     loader.style.setProperty('display', 'block');
+}
+
+function hideLoader() {
+    const loader = document.getElementsByClassName("loader")[0];
+    loader.style.setProperty('display', 'none');
+}
+
+function triggerError(errorMessage) {
+    const alertArea = document.getElementsByClassName("alert")[0];
+    alertArea.style.setProperty('background-color', "#f44336");
+
+    const errorText = "ПАМЫЛКА: " + errorMessage;
+    alertArea.innerHTML = alertArea.innerHTML.replace('\#', errorText);
+
+    alertArea.style.setProperty('display', 'block');
+
+    setTimeout(() => {
+        alertArea.style.setProperty('display', 'none');
+        alertArea.innerHTML = alertArea.innerHTML.replace(errorText, "#");
+    }, 6000);
+}
+
+function triggerBadCharactersWarning(badCharacters) {
+    const alertArea = document.getElementsByClassName("alert")[0];
+    alertArea.style.setProperty('background-color', "#ff9800");
+    alertArea.style.setProperty('display', 'block');
+
+    const errorText = "УВАГА: вамі былі ўведзены сімвалы, якія не ўваходзяць у беларускі алфавіт ці ў знакі прыпынку: " + badCharacters + "  Яны выдалены.";
+    alertArea.innerHTML = alertArea.innerHTML.replace('\#', errorText);
+    setTimeout(() => {
+        alertArea.style.setProperty('display', 'none');
+        alertArea.innerHTML = alertArea.innerHTML.replace(errorText, "#");
+    }, 6000);
+
+}
+
+function disableSendingButton() {
+    document.getElementById('submit-text').disabled = true;
+}
+
+function enableSendingButton() {
+    document.getElementById('submit-text').disabled = false;
+}
+button.addEventListener("click", function (e) {
+    const submittedText = document.getElementById('submitting_form').value;
+    
+    const preprocessingResult = processText(submittedText);
+    if (preprocessingResult.error === true) {
+        triggerError(preprocessingResult.text);
+        return ;
+    }
+    else if (preprocessingResult.error === false && "badCharacters" in preprocessingResult) {
+        triggerBadCharactersWarning(preprocessingResult.badCharacters);
+    }
+    showLoader();
+    disableSendingButton();
     axios({
         url: `${protocol}://${hostname}/tts`,
         method: 'post',
         headers: { 'Content-Type': 'application/json' },
         data: {
-            text: submitted_text
+            text: preprocessingResult.text
         }
     }).then(response => {
         const filename = response.data.audio_name;
@@ -34,7 +91,7 @@ button.addEventListener("click", function (e) {
         const er_mes = error.message;
         console.log('error in getting response with the file name: ' + er_mes)
     }).finally(() => {
-        loader.style.setProperty('display', 'none');
+        enableSendingButton();
+        hideLoader();
     })
-
 });
---
+++ static/modules/process-text.js
@@ -0,0 +1,35 @@
+const defaultText = "Прывітанне! Увядзіце свой тэкст, каб агучыць яго, і клікніце на кнопку, ці проста клікніце на кнопку ніжэй.";
+const allowedCharacters = "АБВГДЕЁЖЗІЙКЛМНОПРСТУЎФХЦЧШЫЬЭЮЯабвгдеёжзійклмнопрстуўфхцчшыьэюя!'(),-.:;? ";
+
+export function processText(text) {
+    if (text.length > 300) {
+        return { error: true, text: "too long" };
+    }
+    if (text.length === 0) {
+        return { error: false, text: defaultText };
+    }
+    text = text.replaceAll('и', 'і');
+    text = text.replaceAll('И', 'І');
+
+    let badCharacters = "";
+    for (const symbol of text) {
+        if (allowedCharacters.search(symbol) === -1) {
+            badCharacters += symbol;
+        }
+    }
+    
+    if (badCharacters.length >= text.length / 2) {
+        return {error: true, text: "занадта шмат сімвалаў не на беларускай мове."}
+    }
+
+    for (const badCharacter of badCharacters) {
+        text = text.replaceAll(badCharacter, '');
+    }
+
+    if (badCharacters.length !== 0) {
+        return { error: false, text: text, badCharacters: badCharacters };
+    }
+
+    return {error: false, text: text};
+    
+}
