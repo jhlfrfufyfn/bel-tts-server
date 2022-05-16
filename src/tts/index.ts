56c193c9458bbb27e79233009a090300c6175697
jhlfrfufyfn <jhlfrfufyfn@gmail.com>
2022-05-16 14:20:12 +0300

    feat: updated file paths to a new model

--- src/tts/index.ts
+++ src/tts/index.ts
@@ -4,22 +4,34 @@ import { v4 as uuidv4 } from 'uuid';
 import path from 'node:path';
 const router = express.Router();
 
-const model_path: string = process.env.MODEL_PATH || "model.pth.tar";
-const config_path: string = process.env.CONFIG_PATH || "config.json";
-const audio_path: string = process.env.AUDIO_PATH || "audio";
-const static_path: string = process.env.STATIC_PATH || "static";
-// define the home page route
+const pathToModel: string = process.env.MODEL_PATH || "/app";
+const audioPath: string = process.env.AUDIO_PATH || "/app";
+const staticPath: string = process.env.STATIC_PATH || "/app";
+
+function generateRandomString(): string {
+  return uuidv4();
+}
+// defining the home page route
 router.get('/', function (request: express.Request, response: express.Response) {
-  response.sendFile(path.join(static_path, 'tts.html'));
+  response.sendFile(path.join(staticPath, 'tts.html'));
 });
 
 router.post('/', function (request: express.Request, response: express.Response) {
     const text: string = request.body.text;
+
     console.log("text: " + text);
-    const filename = uuidv4() + ".wav";
-    const output_path: string = path.join(audio_path, filename);
+
+    const filename = generateRandomString() + ".wav";
+
+    const outputPath: string = path.join(audioPath, filename);
+    const modelPath: string = path.join(pathToModel, 'model.pth');
+    const configPath: string = path.join(pathToModel, 'config.json');
+    const vocoderPath: string = path.join(pathToModel, 'vocoder.pth');
+    const vocoderConfigPath: string = path.join(pathToModel, 'vocoder-config.json');
+
     console.log('before gen');
-    const generate_audio = spawn('tts', ['--text', text, '--model_path', model_path, '--config_path', config_path, '--out_path', output_path]);
+
+    const generate_audio = spawn('tts', ['--text', text, '--model_path', modelPath, '--config_path', configPath, '--vocoder_path', vocoderPath, '--vocoder_config_path', vocoderConfigPath, '--out_path', outputPath]);
 
     generate_audio.on('error', (error) => {
       console.error('error: ' + error.message);
@@ -45,5 +57,7 @@ router.post('/', function (request: express.Request, response: express.Response)
       }
     });
   });
+
+
 
 export default router;
